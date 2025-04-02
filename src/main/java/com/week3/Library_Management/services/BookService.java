b483d16ecee343a9b6d42209995e1e02a2015f7d
Mritunjay Pratap Singh <mritunjaypratapsinghh@gmail.com>
2025-04-02 12:46:31 +0530

    Created book services:

--- src/main/java/com/week3/Library_Management/services/BookService.java
+++ src/main/java/com/week3/Library_Management/services/BookService.java
@@ -1,7 +1,40 @@
 package com.week3.Library_Management.services;
 
+import com.week3.Library_Management.dto.BookDTO;
+import com.week3.Library_Management.entities.BookEntity;
+import com.week3.Library_Management.repositories.BookRepositories;
 import org.springframework.stereotype.Service;
-
+import org.modelmapper.ModelMapper;
 @Service
 public class BookService {
+
+    private final ModelMapper modelMapper;
+    private final BookRepositories bookRepositories;
+
+    public BookService(ModelMapper modelMapper, BookRepositories bookRepositories){
+        this.modelMapper = modelMapper;
+        this.bookRepositories = bookRepositories;
+    }
+
+
+    public void exitsBookById(Long bookId){
+        boolean exists = bookRepositories.existsById(bookId);
+        if(!exists) throw new RuntimeException("Book does not exist with Id: "+ bookId);
+    }
+
+    public BookDTO getBookById(Long bookId) {
+        exitsBookById(bookId);
+        BookEntity bookEntity = bookRepositories.findById(bookId).get();
+        return modelMapper.map(bookEntity,BookDTO.class);
+    }
+
+    public BookDTO createNewBook(BookDTO request){
+        BookEntity bookEntity = modelMapper.map(request,BookEntity.class);
+        BookEntity newBook = bookRepositories.save(bookEntity);
+        return modelMapper.map(newBook,BookDTO.class);
+    }
+
+
+
+
 }
