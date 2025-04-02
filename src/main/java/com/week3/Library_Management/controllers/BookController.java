f0d2d118fc9479701cbe4842c0ca53228398e625
Mritunjay Pratap Singh <mritunjaypratapsinghh@gmail.com>
2025-04-02 12:06:34 +0530

    Added Constructor Dependency Injection of BookService

--- src/main/java/com/week3/Library_Management/controllers/BookController.java
+++ src/main/java/com/week3/Library_Management/controllers/BookController.java
@@ -1,11 +1,19 @@
 package com.week3.Library_Management.controllers;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.week3.Library_Management.dto.BookDTO;
+import com.week3.Library_Management.services.BookService;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path = "/book")
 public class BookController {
 
+    private final BookService bookService;
+
+    public BookController(BookService bookService){
+        this.bookService = bookService;
+    }
+
 }
+
