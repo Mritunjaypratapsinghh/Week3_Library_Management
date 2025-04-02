3687515e6c79fd3674957a43685834a5fc216160
Mritunjay Pratap Singh <mritunjaypratapsinghh@gmail.com>
2025-04-02 12:40:08 +0530

    used repository annotation

--- src/main/java/com/week3/Library_Management/repositories/AuthorRepositories.java
+++ src/main/java/com/week3/Library_Management/repositories/AuthorRepositories.java
@@ -2,6 +2,8 @@ package com.week3.Library_Management.repositories;
 
 import com.week3.Library_Management.entities.AuthorEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AuthorRepositories extends JpaRepository<AuthorEntity,Long>{
 }
--- src/main/java/com/week3/Library_Management/repositories/BookRepositories.java
+++ src/main/java/com/week3/Library_Management/repositories/BookRepositories.java
@@ -2,6 +2,8 @@ package com.week3.Library_Management.repositories;
 
 import com.week3.Library_Management.entities.BookEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface BookRepositories extends JpaRepository<BookEntity,Long> {
 }
