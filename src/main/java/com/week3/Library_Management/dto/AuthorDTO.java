54ad7211ef765f6f757fe764c2d631b98f299412
Mritunjay Pratap Singh <mritunjaypratapsinghh@gmail.com>
2025-04-02 12:47:05 +0530

    Added Lombok Annotation

--- src/main/java/com/week3/Library_Management/dto/AuthorDTO.java
+++ src/main/java/com/week3/Library_Management/dto/AuthorDTO.java
@@ -1,9 +1,15 @@
 package com.week3.Library_Management.dto;
 
 import com.week3.Library_Management.entities.BookEntity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Set;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class AuthorDTO {
 
     private Long id;
--- src/main/java/com/week3/Library_Management/dto/BookDTO.java
+++ src/main/java/com/week3/Library_Management/dto/BookDTO.java
@@ -1,7 +1,13 @@
 package com.week3.Library_Management.dto;
 
 import com.week3.Library_Management.entities.AuthorEntity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class BookDTO {
 
     private Long id;
