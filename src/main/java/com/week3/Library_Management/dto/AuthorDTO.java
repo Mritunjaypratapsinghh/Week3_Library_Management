732558cfca6a889a7dbd0489138698c6dbdafc05
Mritunjay Pratap Singh <mritunjaypratapsinghh@gmail.com>
2025-04-02 12:04:49 +0530

    Updated DTO for both entities

--- src/main/java/com/week3/Library_Management/dto/AuthorDTO.java
+++ src/main/java/com/week3/Library_Management/dto/AuthorDTO.java
@@ -1,4 +1,12 @@
 package com.week3.Library_Management.dto;
 
+import com.week3.Library_Management.entities.BookEntity;
+
+import java.util.Set;
+
 public class AuthorDTO {
+
+    private Long id;
+    private String name;
+    private Set<BookDTO> books;
 }
--- src/main/java/com/week3/Library_Management/dto/BookDTO.java
+++ src/main/java/com/week3/Library_Management/dto/BookDTO.java
@@ -1,4 +1,10 @@
 package com.week3.Library_Management.dto;
 
+import com.week3.Library_Management.entities.AuthorEntity;
+
 public class BookDTO {
+
+    private Long id;
+    private String name;
+    private AuthorDTO author;
 }
